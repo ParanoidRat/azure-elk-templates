d568b5a3635c016dd2900cb44bbfa3016162d0f0
Paranoid Rat <paranoid.rat@gmail.com>
2017-04-19 21:49:47 -0400

    Polished Logstash bootstrap

--- scripts/logstash-ubuntu-install.sh
+++ scripts/logstash-ubuntu-install.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+# see http://redsymbol.net/articles/unofficial-bash-strict-mode/
+# for explanation of set options
+set -euo pipefail
+IFS=$'\n\t'
 
 #########################
 # Global vars
 #########################
+DEBUG=1
 
+# Console output text colors
+BOLD=$(tput bold)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+NORMAL=$(tput sgr0)
+
+# Script parameters
 LOGSTASH_VERSION="5.3.0"
 INSTALL_ADDITIONAL_PLUGINS=""
 ES_URI="http://10.0.0.4:9200"
@@ -34,47 +46,67 @@ help()
 log()
 {
     echo \[$(date '+%Y-%m-%d %H:%M:%S')\] "$1"
-    echo \[$(date '+%Y-%m-%d %H:%M:%S')\] "$1" >> /var/log/arm-install.log
+    if [ ! $DEBUG ]; then echo \[$(date '+%Y-%m-%d %H:%M:%S')\] "$1" >> /var/log/arm-install.log; fi
+}
+
+run_cmd()
+{
+  # temporarily disable the exit-immediately-on-error option
+  set +e
+  (
+    # execute command
+    if eval "$@"; then
+      if [ $DEBUG ]; then log "[run_cmd]${GREEN}[+] $@ ${NORMAL}"; fi  
+    else
+      log "[run_cmd] [run_cmd]${BOLD}${RED}[!] $@ ${NORMAL}"
+    fi
+  )
+  # re-enable the exit-immediately-on-error option
+  set -e
 }
 
 install_java()
 {
-    log "[install_java] Adding apt repository for java 8"
-    (add-apt-repository -y ppa:webupd8team/java || (sleep 15; add-apt-repository -y ppa:webupd8team/java))
-    log "[install_java] updating apt-get"
+    log "[install_java] adding APT repository for oracle-java8..."
+    run_cmd "(add-apt-repository -y ppa:webupd8team/java || (sleep 15; add-apt-repository -y ppa:webupd8team/java))"
+    
+    log "[install_java] 'apt-get update' started..."
+    run_cmd "(apt-get -y update || (sleep 15; apt-get -y update)) > /dev/null"
 
-    (apt-get -y update || (sleep 15; apt-get -y update)) > /dev/null
-    log "[install_java] updated apt-get"
-    echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-    echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-    log "[install_java] Installing Java"
-    (apt-get -yq install oracle-java8-installer || (sleep 15; apt-get -yq install oracle-java8-installer))
-    command -v java >/dev/null 2>&1 || { sleep 15; sudo rm /var/cache/oracle-jdk8-installer/jdk-*; sudo apt-get install -f; }
+    log "[install_java] accepting oracle license..."
+    run_cmd "(echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections)"
+    run_cmd "(echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections)"
+    
+    log "[install_java] oracle-java8 install started..."
+    run_cmd "(apt-get -yq install oracle-java8-installer || (sleep 15; apt-get -yq install oracle-java8-installer))"
+    run_cmd "(command -v java >/dev/null 2>&1 || { sleep 15; rm /var/cache/oracle-jdk8-installer/jdk-*; apt-get install -f; })"
 
     #if the previous did not install correctly we go nuclear, otherwise this loop will early exit
-    for i in $(seq 30); do
+    for i in $(seq 4); do
       if $(command -v java >/dev/null 2>&1); then
-        log "[install_java] Installed java!"
+        log "[install_java] oracle-java8 install ended"
         return
       else
-        sleep 5
-        sudo rm /var/cache/oracle-jdk8-installer/jdk-*;
-        sudo rm -f /var/lib/dpkg/info/oracle-java8-installer*
-        sudo rm /etc/apt/sources.list.d/*java*
-        sudo apt-get -yq purge oracle-java8-installer*
-        sudo apt-get -yq autoremove
-        sudo apt-get -yq clean
-        (add-apt-repository -y ppa:webupd8team/java || (sleep 15; add-apt-repository -y ppa:webupd8team/java))
-        sudo apt-get -yq update
-        sudo apt-get -yq install --reinstall oracle-java8-installer
-        log "[install_java] Seeing if java is Installed after nuclear retry ${i}/30"
+        log "[install_java] oracle-java8 install NOT successful, going nuclear..."
+        run_cmd "(sleep 5)"
+        run_cmd "(rm /var/cache/oracle-jdk8-installer/jdk-*;)"
+        run_cmd "(rm -f /var/lib/dpkg/info/oracle-java8-installer*)"
+        run_cmd "(rm /etc/apt/sources.list.d/*java*)"
+        run_cmd "(apt-get -yq purge oracle-java8-installer*)"
+        run_cmd "(apt-get -yq autoremove)"
+        run_cmd "(apt-get -yq clean)"
+        run_cmd "(add-apt-repository -y ppa:webupd8team/java || (sleep 15; add-apt-repository -y ppa:webupd8team/java))"
+        run_cmd "(apt-get -yq update)"
+        run_cmd "(apt-get -yq install --reinstall oracle-java8-installer)"
+        log "[install_java] Seeing if oracle-java8 is installed after nuclear retry ${i}/4"
       fi
     done
-    command -v java >/dev/null 2>&1 || { log "Java did not get installed properly even after a retry and a forced installation" >&2; exit 50; }
+    command -v java >/dev/null 2>&1 || { log "oracle-java8 install NOT successful after 4 forced re-installation retries, ABORT! ABORT!" >&2; exit 50; }
 }
 
 install_logstash()
 {
+    log "[install_logstash] Logstash $LOGSTASH_VERSION install started..."
     if [[ "${LOGSTASH_VERSION}" == \2* ]]; then
         DOWNLOAD_URL="https://download.elastic.co/logstash/logstash/packages/debian/logstash-$LOGSTASH_VERSION_all.deb"
     elif [[ "${LOGSTASH_VERSION}" == \5* ]]; then
@@ -83,15 +115,17 @@ install_logstash()
         DOWNLOAD_URL="https://artifacts.elastic.co/downloads/logstash/logstash-$LOGSTASH_VERSION.deb"
     fi
 
-    log "[install_logstash] Installing logstash version - $LOGSTASH_VERSION"
-    log "[install_logstash] Download location - $DOWNLOAD_URL"
-    sudo wget -q "$DOWNLOAD_URL" -O logstash.deb
-    log "[install_logstash] Downloaded logstash $LOGSTASH_VERSION"
-    sudo dpkg -i logstash.deb
-    log "[install_logstash] Installed logstash version - $LOGSTASH_VERSION"
+    log "[install_logstash] Downloading .deb package @ $DOWNLOAD_URL ..."
+    run_cmd "(wget -q '$DOWNLOAD_URL' -O logstash-$LOGSTASH_VERSION.deb)"
+    
+    log "[install_logstash] Installing logstash-$LOGSTASH_VERSION.deb ..."
+    run_cmd "(dpkg -i logstash-$LOGSTASH_VERSION.deb)"
 
-    log "[install_logstash] Disable logstash System-V style init scripts (will be using monit)"
-    sudo update-rc.d logstash disable
+
+    if [[ "${LOGSTASH_VERSION}" == \2* ]]; then
+      log "[install_logstash] Disable Logstash SysV init scripts (will be using monit)"
+      run_cmd "(update-rc.d logstash disable)"
+    fi
 }
 
 plugin_cmd()
@@ -105,32 +139,35 @@ plugin_cmd()
 
 install_additional_plugins()
 {
-    SKIP_PLUGINS="license"
-    log "[install_additional_plugins] Installing additional plugins"
+    local SKIP_PLUGINS="license"
+ 
+    log "[install_additional_plugins] Additional Logstash plugins install started..."
+    
     for PLUGIN in $(echo $INSTALL_ADDITIONAL_PLUGINS | tr ";" "\n")
     do
         if [[ $SKIP_PLUGINS =~ $PLUGIN ]]; then
-            log "[install_additional_plugins] Skipping plugin $PLUGIN"
+            log "[install_additional_plugins] Skipping plugin '$PLUGIN'"
         else
-            log "[install_additional_plugins] Installing plugin $PLUGIN"
-            sudo $(plugin_cmd) install $PLUGIN
-            log "[install_additional_plugins] Installed plugin $PLUGIN"
+            log "[install_additional_plugins] Install for '$PLUGIN' plugin started..."
+            run_cmd "($(plugin_cmd) install $PLUGIN)"
         fi
     done
 }
 
 configure_logstash_yml()
 {
-
     local LOGSTASH_CONF=/etc/logstash/logstash.yml
 
-    log "[configure_logstash_yml] Configuring Logstash $LOGSTASH_CONF"
+    log "[configure_logstash_yml] Logstash configuration started..."
 
     log "[configure_logstash_yml] Backup old $LOGSTASH_CONF"
     mv $LOGSTASH_CONF $LOGSTASH_CONF.bak
 
-    log "[configure_logstash_yml] Pointing output to $ES_URI"
-    if [[ "${ES_VERSION}" == \5* ]]; then
+    log "[configure_logstash_yml] Redis input defined as '$REDIS_HOST:$REDIS_PORT'"
+    log "[configure_logstash_yml] Redis channel defined as '$REDIS_KEY'"
+    log "[configure_logstash_yml] Elasticsearch output URI defined as '$ES_URI'"
+
+    if [[ "${LOGSTASH_VERSION}" == \5* ]]; then
       {
           echo -e "# Get input from Redis bufffer"
           echo -e "input {"
@@ -151,8 +188,18 @@ configure_logstash_yml()
       } >> $LOGSTASH_CONF
     else
       {
+          echo -e "# Get input from Redis bufffer"
+          echo -e "input {"
+          echo -e "  redis {"
+          echo -e "    host => \"$REDIS_HOST\""
+          echo -e "    port => \"$REDIS_PORT\""
+          echo -e "    password => \"$REDIS_PASSWORD\""
+          echo -e "    data_type => \"channel\""
+          echo -e "    key => \"$REDIS_KEY\""
+          echo -e "  }"
+          echo -e "}"
           echo -e ""
-          echo -e "# output to Elasticsearch cluster"
+          echo -e "# Send output to Elasticsearch cluster"
           echo -e "output {"
           echo -e "  elasticsearch { hosts => [\"$ES_URI\"] }"
           echo -e "}"
@@ -161,45 +208,76 @@ configure_logstash_yml()
     fi 
 }
 
-install_ntp()
+configure_logstash_monit()
 {
-    log "[install_ntp] installing ntp daemon"
-    (apt-get -yq install ntp || (sleep 15; apt-get -yq install ntp))
-    ntpdate pool.ntp.org
-    log "[install_ntp] installed ntp daemon and ntpdate"
+    local MONIT_LS_CONF=/etc/monit/conf.d/logstash.conf
+    
+    log "[configure_logstash_monit] Generating Logstash config for Monit @ $MONIT_LS_CONF"
+    run_cmd "(touch $MONIT_LS_CONF)"
+    {
+      echo -e "check process logstash matching \"logstash/runner.rb\""
+      echo -e "  group logstash"
+      echo -e "  start program = \"/bin/systemctl start logstash.service\""
+      echo -e "  stop program = \"/bin/systemctl start logstash.service\""
+    } > $MONIT_LS_CONF      
+
+    log "[configure_logstash_monit] Reloading Monit and starting services..."
+    run_cmd "(monit reload)"
+    run_cmd "(monit start all)"
 }
 
 install_monit()
 {
-    log "[install_monit] installing monit"
-    (apt-get -yq install monit || (sleep 15; apt-get -yq install monit))
-    log "[install_monit] installed monit"
-
     local MONIT_CONF=/etc/monit/monitrc
-    log "[install_monit] Appending Monit $MONIT_CONF"
-    echo "set daemon 30" >> $MONIT_CONF
-    echo "set httpd port 2812 and" >> $MONIT_CONF
-    echo "    use address localhost" >> $MONIT_CONF
-    echo "    allow localhost" >> $MONIT_CONF
 
-    local MONIT_LS_CONF=/etc/monit/conf.d/logstash.conf
-    log "[install_monit] Creating Monit config for Logstash $MONIT_LS_CONF"
-    sudo touch $MONIT_LS_CONF
-    echo "check process logstash with pidfile \"/var/run/logstash/logstash.pid\"" >> $MONIT_LS_CONF
-    echo "  group logstash" >> $MONIT_LS_CONF
-    echo "  start program = \"/etc/init.d/logstash start\"" >> $MONIT_LS_CONF
-    echo "  stop program = \"/etc/init.d/logstash stop\"" >> $MONIT_LS_CONF
-    log "[install_monit] configured monit"
+
+    log "[install_monit] Monit install started..."
+    run_cmd "(apt-get -yq install monit || (sleep 15; apt-get -yq install monit))"
+    
+    log "[install_monit] Appending Monit configuration @ $MONIT_CONF ..."
+    {
+      echo -e "set daemon 30"
+      echo -e "set httpd port 2812 and"
+      echo -e "    use address localhost"
+      echo -e "    allow localhost" 
+    } >> $MONIT_CONF
+
+    
+
+    log "[install_monit] Starting monit..."
+    run_cmd "(systemctl start monit.service)"
+
 }
 
-start_monit()
+fix_hostname()
 {
-    log "[start_monit] starting monit"
-    sudo /etc/init.d/monit start
-    sudo monit reload # use the new configuration
-    sudo monit start all
-    log "[start_monit] started monit"
+  log "Fixing hostname in /etc/hosts..."
+  
+  set -e
+  (
+    grep -q "${HOSTNAME}" /etc/hosts
+  )
+
+  if [ $? == 0 ]; then
+    log "${HOSTNAME} found in /etc/hosts"
+  else
+    log "${HOSTNAME} not found in /etc/hosts"
+    # Append it to the hsots file if not there
+    run_cmd "(echo \"127.0.0.1 ${HOSTNAME}\" >> /etc/hosts)"
+    log "hostname ${HOSTNAME} added to /etc/hosts"
+  fi
 }
+
+#########################
+# Check requirements
+#########################
+
+if [ "${UID}" -ne 0 ]; then
+    echo "Script executed without root permissions"
+    echo "You must be root to run this program." >&2
+    exit 3
+fi
+
 
 #########################
 # Main
@@ -217,7 +295,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 #Loop through options passed
 while getopts :V:L:U:R:P:W:K:h optname; do
-  log "Option $optname set"
+  log "Option $optname set to '${OPTARG}'"
   case $optname in
     V) #Logstash version number
       LOGSTASH_VERSION=${OPTARG}
@@ -236,6 +314,7 @@ while getopts :V:L:U:R:P:W:K:h optname; do
       ;;
     W) #Redis password
       REDIS_PASSWORD="${OPTARG}"
+      ;;      
     K) #Redis list/channel
       REDIS_KEY="${OPTARG}"
       ;;
@@ -244,7 +323,7 @@ while getopts :V:L:U:R:P:W:K:h optname; do
       exit 2
       ;;
     \?) #unrecognized option - show help
-      echo -e \\n"Option -${BOLD}$OPTARG${NORM} not allowed."
+      echo -e \\n"Option -${BOLD}$OPTARG${NORMAL} not allowed."
       help
       exit 2
       ;;
@@ -252,32 +331,7 @@ while getopts :V:L:U:R:P:W:K:h optname; do
 done
 
 
-#########################
-# Check requirements
-#########################
-
-if [ "${UID}" -ne 0 ];
-then
-    log "Script executed without root permissions"
-    echo "You must be root to run this program." >&2
-    exit 3
-fi
-
-# TEMP FIX - Re-evaluate and remove when possible
-# This is an interim fix for hostname resolution in current VM
-grep -q "${HOSTNAME}" /etc/hosts
-if [ $? == 0 ]
-then
-  log "${HOSTNAME}found in /etc/hosts"
-else
-  log "${HOSTNAME} not found in /etc/hosts"
-  # Append it to the hsots file if not there
-  echo "127.0.0.1 ${HOSTNAME}" >> /etc/hosts
-  log "hostname ${HOSTNAME} added to /etchosts"
-fi
-
-log "Bootstrapping Logstash $LOGSTASH_VERSION"
-
+log "Bootstrapping Logstash..."
 
 #########################
 # Installation sequence
@@ -286,22 +340,22 @@ log "Bootstrapping Logstash $LOGSTASH_VERSION"
 
 # if logstash is already installed assume this is a redeploy
 # change yaml configuration and only restart the server when needed
-if sudo monit status logstash >& /dev/null; then
-
+if monit status logstash >& /dev/null; then
   configure_logstash_yml
 
   # restart logstash if the configuration has changed
   cmp --silent /etc/logstash/logstash.yml /etc/logstash/logstash.bak \
-    || sudo monit restart logstash
+    || monit restart logstash
 
   exit 0
 fi
 
-# install dependencies
-install_ntp
+fix_hostname
+
+install_monit
+
 install_java
 
-# install logstash 
 install_logstash
 
 # install additional plugins for logstash if necessary
@@ -309,14 +363,11 @@ if [[ ! -z "${INSTALL_ADDITIONAL_PLUGINS// }" ]]; then
     install_additional_plugins
 fi
 
-# install monit to make sure logstash is always running
-install_monit
-
-# configure logstash
 configure_logstash_yml
 
-# start monit daemon and logstash
-start_monit
+# Logstash started by SystemD (Ubuntu 16.04) does not record main Java process PID.
+# As such, Monit could not reliably monitor it
+#configure_logstash_monit
 
 ELAPSED_TIME=$(($SECONDS - $START_TIME))
 PRETTY=$(printf '%dh:%dm:%ds\n' $(($ELAPSED_TIME/3600)) $(($ELAPSED_TIME%3600/60)) $(($ELAPSED_TIME%60)))
